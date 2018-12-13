b14569f1bfb516ecc20490f253daba26761f2a4e
steven.kore <steven.kore@khk.ee>
2018-12-13 13:06:53 +0200

    Väljastamine ja pushimine

---
+++ print/index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: steven.kore
+ * Date: 13.12.2018
+ * Time: 12:46
+ */
+// teksti väljastamine
+// print
+print('Tere maailm!<br>');
+print('PHP alused<br>');
+// echo
+echo 'nii toimub väljastamine<br>';
+echo 'Tere<br>';
+// väärtuste ühendamine
+echo 'See '.'on'.' kolmest sõnast kokku pantud<br>';
+
+
+
+
+?>
