f6edf4c661eb44e1eb171339ed0463071fb074ec
steven.kore <steven.kore@khk.ee>
2018-12-14 13:12:21 +0200

    muutjad

--- Condition/index.php
+++ Condition/index.php
@@ -26,17 +26,56 @@ if($arv % 2 == 0){
 <?php
 
 echo '<h5>Ülesanne 2</h5>';
-$arv = rand(0, 100);
-if($arv >= 0 and $arv < 25){
-    echo '<div style="color: green">'.$arv.'</div>';
-} else if($arv >= 25 and $arv < 50) {
-    echo '<div style="color: red">'.$arv.'</div>';
-} else if($arv >= 50 and $arv < 75) {
-    echo '<div style="color: blue">'.$arv.'</div>';
-} else if($arv >= 75 and $arv < 100) {
-    echo '<div style="color: orange">'.$arv.'</div>';
+$arv1 = rand(0, 100);
+if($arv1 >= 0 and $arv1 < 25){
+    echo '<div style="color: green">'.$arv1.'</div>';
+} else if($arv1 >= 25 and $arv1 < 50) {
+    echo '<div style="color: red">'.$arv1.'</div>';
+} else if($arv1 >= 50 and $arv1 < 75) {
+    echo '<div style="color: blue">'.$arv1.'</div>';
+} else if($arv1 >= 75 and $arv1 < 100) {
+    echo '<div style="color: orange">'.$arv1.'</div>';
+} else {
+    echo '<div style="color: black">'.$arv1.'</div>';
 }
+?>
 
+    <style>
+        div{
+            width: 100px;
+            height: 100px;
+            border-radius: 50px;
+            border: solid 1px black;
+        }
+    </style>
+
+<?php
+
+
+echo '<h5>Ülesanne 3</h5>';
+$aktiivne = 'punane';
+switch ($aktiivne){
+    case 'punane':
+        echo '<div style="background: red"></div>';
+        echo '<div></div>';
+        echo '<div></div>';
+        break;
+    case 'kollane':
+        echo '<div></div>';
+        echo '<div style="background: orange"></div>';
+        echo '<div></div>';
+        break;
+    case 'roheline':
+        echo '<div></div>';
+        echo '<div></div>';
+        echo '<div style="background: green"></div>';
+        break;
+    default:
+        echo '<div style="background: black"></div>';
+        echo '<div style="background: black"></div>';
+        echo '<div style="background: black"></div>';
+        break;
+}
 
 
 ?>
