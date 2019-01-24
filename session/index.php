3e94a8bde9b28a9050328151d12443d454e05a82
steven.kore <steven.kore@khk.ee>
2019-01-24 15:33:35 +0200

    session sisse logimine ja välja logimine.

--- session/index.php
+++ session/index.php
@@ -3,6 +3,7 @@ session_start();
 require_once 'fnc.php';
 if(isset($_SESSION['user'])){
     echo 'Tere tulemast, '.$_SESSION['user']['last_name'].'!<br>';
+    echo '<a href="acts/logout.php">Logi välja</a>';
 } else {
     $loginForm = htmlFromFile('login');
     echo $loginForm;
