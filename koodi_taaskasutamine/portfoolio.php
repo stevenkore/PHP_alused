bb747b472e6ab13273205a99f30de48d082ecb4b
steven.kore <steven.kore@khk.ee>
2019-01-29 11:55:45 +0200

    Portfoolio

---
+++ koodi_taaskasutamine/portfoolio.php
@@ -0,0 +1,18 @@
+
+<h1>Portfoolio</h1>
+
+<style>
+    #minuCanvas {
+        background-image: url("http://chandra.harvard.edu/photo/2019/cyga/cyga_250.jpg");
+    }
+</style>
+<canvas id="minuCanvas" width="250" height="250" style="border:1px solid #000;">
+
+</canvas>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: steven.kore
+ * Date: 29.01.2019
+ * Time: 11:41
+ */
