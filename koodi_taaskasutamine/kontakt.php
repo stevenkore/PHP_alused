066d5b36b640c07c6e045a700f39bfc1d1660493
steven.kore <steven.kore@khk.ee>
2019-01-29 11:55:16 +0200

    Kontakt leht

---
+++ koodi_taaskasutamine/kontakt.php
@@ -0,0 +1,11 @@
+<h1>Kontakt</h1>
+<br><br>
+<img src="https://cdn4.iconfinder.com/data/icons/rcons-phone/16/handset_round-2-512.png" width="200" height="200"/>
+<h3>+3724343242</h3>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: steven.kore
+ * Date: 29.01.2019
+ * Time: 11:50
+ */
