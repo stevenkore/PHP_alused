1872bf72a712f4499fe7bf2a8826242827e8bdc6
steven.kore <steven.kore@khk.ee>
2019-01-29 11:54:48 +0200

    Leht koos kaartiga.

---
+++ koodi_taaskasutamine/kaart.php
@@ -0,0 +1,9 @@
+<h1>Kaart</h1>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2093.611282635123!2d26.712516316290305!3d58.34935909624493!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x46eb37487c7817f9%3A0x31f91313d1bbe0ee!2sTartu+Kutsehariduskeskus!5e0!3m2!1set!2see!4v1548755321930" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: steven.kore
+ * Date: 29.01.2019
+ * Time: 11:48
+ */
